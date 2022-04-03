e8592f417b5e33a3eb5815ed1d4e869db6b6e5f0
Michael <m24572582@gmail.com>
2022-04-04 02:01:55 +0800

    發射子彈

--- app/src/main/java/com/thunder/thunderplane/MainActivity.kt
+++ app/src/main/java/com/thunder/thunderplane/MainActivity.kt
@@ -2,6 +2,8 @@ package com.thunder.thunderplane
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.MotionEvent
 import android.view.View
 import androidx.activity.viewModels
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.thunder.thunderplane.databinding.ActivityMainBinding
 import com.thunder.thunderplane.log.MichaelLog
+import java.nio.channels.FileLock
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,11 +22,43 @@ class MainActivity : AppCompatActivity() {
     private var y = 0f
     private var rawX = 0f
     private var rawY = 0f
-
+    private val bulletArray = mutableListOf<View>()
+    private val handler = Handler(Looper.myLooper()!!)
+    private lateinit var bullet: View
     private val viewModel: MainViewModel by viewModels {
         MainViewModel.MainViewModelFactory()
     }
 
+
+
+    private fun reShooting() {
+        handler.postDelayed(Runnable {
+            bullet = View.inflate(this, R.layout.bullet_layout, null)
+            dataBinding.root.addView(bullet)
+
+            bullet.visibility = View.INVISIBLE
+
+            bullet.post {
+                val centerX = (dataBinding.jet.x + ((dataBinding.jet.right - dataBinding.jet.left) / 2)) - ((bullet.right - bullet.left) / 2)
+                bullet.x = centerX
+                bullet.y = dataBinding.jet.y
+                bullet.visibility = View.VISIBLE
+                startShooting()
+                reShooting()
+            }
+        }, 1000)
+    }
+
+    private fun startShooting() {
+        handler.postDelayed(Runnable {
+            bullet.animate().y(bullet.y - 50f).setDuration(0).start()
+            startShooting()
+        }, 50)
+    }
+
+    /**
+     *  先暫時移除控制圈
+     */
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,41 +70,60 @@ class MainActivity : AppCompatActivity() {
 
         dataBinding.root.setOnTouchListener(onTouchListener)
 
+        dataBinding.jet.post {
+
+            handler.postDelayed(Runnable {
+                bullet = View.inflate(this, R.layout.bullet_layout, null)
+                dataBinding.root.addView(bullet)
+
+                bullet.visibility = View.INVISIBLE
+
+                bullet.post {
+                    val centerX = (dataBinding.jet.x + ((dataBinding.jet.right - dataBinding.jet.left) / 2)) - ((bullet.right - bullet.left) / 2)
+                    bullet.x = centerX
+                    bullet.y = dataBinding.jet.y
+                    bullet.visibility = View.VISIBLE
+                    startShooting()
+                    reShooting()
+                }
+            }, 1000)
+        }
+
 
         //設定圈圈
-        viewModel.addControlCircleLiveData.observe(this) {
-
-            if (!it.isShow) {
-                dataBinding.circleTarget.visibility = View.INVISIBLE
-                dataBinding.root.removeView(controlView)
-                return@observe
-            }
-            controlView = View.inflate(this, R.layout.control_layout, null)
-            dataBinding.root.addView(controlView)
-            controlView.visibility = View.INVISIBLE
-            controlView.post {
-                val radius = (controlView.bottom - controlView.top) / 2
-                controlView.x = it.x - radius
-                controlView.y = it.y - radius
-                controlView.visibility = View.VISIBLE
-                controlView.post {
-                    viewModel.setControlViewWidthHeight(controlView.x,controlView.y,controlView.x + controlView.width,controlView.y + controlView.height)
-                }
-            }
-
-
-            val targetRadius = (dataBinding.circleTarget.bottom - dataBinding.circleTarget.top) / 2
-            dataBinding.circleTarget.x = it.x - targetRadius
-            dataBinding.circleTarget.y = it.y - targetRadius
-            dataBinding.circleTarget.visibility = View.VISIBLE
-            MichaelLog.i("x ${it.x - targetRadius} , y : ${it.y - targetRadius}")
-
-            dataBinding.circleTarget.post {
-                x = dataBinding.circleTarget.x - rawX
-                y = dataBinding.circleTarget.y - rawY
-                viewModel.setTarget(x, y)
-            }
-        }
+//        viewModel.addControlCircleLiveData.observe(this) {
+//
+//            if (!it.isShow) {
+//                dataBinding.circleTarget.visibility = View.INVISIBLE
+//                dataBinding.root.removeView(controlView)
+//                return@observe
+//            }
+//            controlView = View.inflate(this, R.layout.control_layout, null)
+//            dataBinding.root.addView(controlView)
+//            controlView.visibility = View.INVISIBLE
+//            controlView.post {
+//                val radius = (controlView.bottom - controlView.top) / 2
+//                controlView.x = it.x - radius
+//                controlView.y = it.y - radius
+//                controlView.visibility = View.VISIBLE
+//                controlView.post {
+//                    viewModel.setControlViewWidthHeight(controlView.x,controlView.y,controlView.x + controlView.width,controlView.y + controlView.height)
+//                }
+//            }
+//
+//
+//            val targetRadius = (dataBinding.circleTarget.bottom - dataBinding.circleTarget.top) / 2
+//            dataBinding.circleTarget.x = it.x - targetRadius
+//            dataBinding.circleTarget.y = it.y - targetRadius
+//            dataBinding.circleTarget.visibility = View.VISIBLE
+//            MichaelLog.i("x ${it.x - targetRadius} , y : ${it.y - targetRadius}")
+//
+//            dataBinding.circleTarget.post {
+//                x = dataBinding.circleTarget.x - rawX
+//                y = dataBinding.circleTarget.y - rawY
+//                viewModel.setTarget(x, y)
+//            }
+//        }
 
         //移動飛機
         viewModel.moveJetLiveData.observe(this) {
@@ -77,9 +131,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         //移動目標圈圈
-        viewModel.moveTargetLiveData.observe(this) {
-            dataBinding.circleTarget.animate().x(it.targetX).y(it.targetY).setDuration(0).start()
-        }
+//        viewModel.moveTargetLiveData.observe(this) {
+//            dataBinding.circleTarget.animate().x(it.targetX).y(it.targetY).setDuration(0).start()
+//        }
     }
 
 
@@ -87,7 +141,7 @@ class MainActivity : AppCompatActivity() {
     private val onTouchListener = View.OnTouchListener { _, event ->
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                viewModel.onAddControlCircleListener(true, event.x, event.y)
+//                viewModel.onAddControlCircleListener(true, event.x, event.y)
 
                 rawX = event.rawX
                 rawY = event.rawY
@@ -97,12 +151,12 @@ class MainActivity : AppCompatActivity() {
                 )
             }
             MotionEvent.ACTION_MOVE -> {
-                viewModel.onMoveTargetListener(
-                    event.rawX,
-                    event.rawY,
-                    dataBinding.circleTarget.width,
-                    dataBinding.circleTarget.height
-                )
+//                viewModel.onMoveTargetListener(
+//                    event.rawX,
+//                    event.rawY,
+//                    dataBinding.circleTarget.width,
+//                    dataBinding.circleTarget.height
+//                )
                 viewModel.onMoveJefListener(
                     event.rawX,
                     event.rawY,
@@ -111,19 +165,10 @@ class MainActivity : AppCompatActivity() {
                 )
 
             }
-            MotionEvent.ACTION_UP -> {
-                viewModel.onAddControlCircleListener(false, 0f, 0f)
-            }
+//            MotionEvent.ACTION_UP -> {
+//                viewModel.onAddControlCircleListener(false, 0f, 0f)
+//            }
         }
         true
-    }
-
-
-
-
-    override fun onPause() {
-        super.onPause()
-        viewModel.onAddControlCircleListener(false, 0f, 0f)
-        viewModel.addControlCircleLiveData.removeObservers(this)
     }
 }
